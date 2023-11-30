61a9464a32c12598e51c2d1df9ac1fe373ff3457
Slrachid <sot.rdsi@gmail.com>
2023-11-30 11:23:57 +0100

    Add templates

--- interface_flask.py
+++ interface_flask.py
@@ -1,5 +1,5 @@
 import blockchain, block, transaction
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from uuid import uuid4
 
 
@@ -12,6 +12,9 @@ node_identifiere = str(uuid4()).replace('-', '')
 # Instantiate the Blockchain
 blockchaine = blockchain.Blockchain()
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('home.html')
 
 @app.route('/mine', methods=['GET'])
 def mine():
@@ -35,11 +38,42 @@ def new_transaction():
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
-        'chain': [block.serialize() for block in blockchaine.chain],
+        'chain': [block.block_data for block in blockchaine.chain],
         'length': len(blockchaine.chain),
     }
     return jsonify(response), 200
 
+
+
+
+@app.route('/create_contract')
+def contract():
+    return render_template('create_contract.html')
+
+# Route to handle form submission and create the contract
+@app.route('/create_contract', methods=['POST'])
+def create_contract():
+    # Get form inputs
+    client_name = request.form['client_name']
+    service_description = request.form['service_description']
+    payment_amount = request.form['payment_amount']
+    
+    # Here, you can create the contract using the inputs
+    # For simplicity, let's just format the contract as a string
+    contract = f"Contract\nClient: {client_name}\nDescription of Service: {service_description}\nPayment: ${payment_amount}"
+    
+    # You can then save this contract to a file or database, or perform any other actions needed
+    
+    return contract
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(host='0.0.0.0', port=5000)
     
