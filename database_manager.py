feb0fdb8ded8c61497d007a033aee73ed1039412
Yassine Ouchna <yassine.ouchna@student-cs.fr>
2023-12-12 21:24:16 +0100

    final interface

---
+++ database_manager.py
@@ -0,0 +1,103 @@
+import sqlite3 as sq
+
+
+
+conn = sq.connect('database.db', check_same_thread=False)
+cur = conn.cursor()
+
+
+class contracts_database():
+    def __init__(self):
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS contracts(contract_id TEXT NOT NULL, author, date date, signature, content)")
+        conn.commit()
+
+    def add_contract(self, contract):  # takes in a transaction object
+        data = contract.data
+        contract_id = contract.hash()
+        cur.execute(
+            f"INSERT INTO contracts VALUES ('{contract_id}', '{data['author']}', '{data['date']}','{contract.signature}', '{data['message']}')")
+        conn.commit()
+
+    def get_contract(self, id):
+        r = cur.execute(
+            f"SELECT contract_id, author, date, signature, content FROM contracts WHERE contract_id='{id}'").fetchone()
+        data = {
+            "contract_id": r[0],
+            "author": r[1],
+            "date": r[2],
+            "signature": r[3],
+            "content": r[4]
+        }
+        conn.commit()
+        return data
+
+    def get_history(self, author):
+        r = cur.execute(
+            f"SELECT contract_id, author, date, signature, content FROM contracts WHERE author='{author}'").fetchall()
+        history = [self.get_contract(contract[0]) for contract in r]
+        return history
+
+
+class users():
+    def __init__(self):
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS users(id, hashed_password)")
+        conn.commit()
+
+    def add_user(self, id, hashed_p):
+        cur.execute(
+            f"INSERT INTO contracts VALUES ('{id}', '{hashed_p}')")
+        conn.commit()
+
+    def get_user(self, id):
+        try:
+            r = cur.execute(
+                f"SELECT id, hashed_password FROM users WHERE id='{id}'").fetchall()
+            conn.commit()
+            if r is None:
+                raise Exception("User not found")
+        except Exception as e:
+            print("An error occurred:", e)
+        return r
+
+
+class blockchain_database():
+    def __init__(self):
+        cur.execute(
+            "CREATE TABLE IF NOT EXISTS blockchain(index_block, proof, date date, transaction_id TEXT NOT NULL, previous_hash)")
+        conn.commit()
+
+    def add_block(self, block):  # takes in a transaction object
+        data = block.block_data
+        for contract in data['transactions']:
+            cur.execute(
+                f"INSERT INTO blockchain VALUES ('{data['index']}', '{data['proof']}', '{data['timestamp']}','{contract.hash()}', '{data['previous_hash']}')")
+            conn.commit()
+
+    def get_block(self, index_block):
+        b = cur.execute(
+            f"SELECT index_block, proof, date, transaction_id, previous_hash FROM blockchain WHERE index_block='{index_block}'").fetchall()
+        data = []
+        for r in b:
+            data.append({
+                "index_block": r[0],
+                "proof": r[1],
+                "date": r[2],
+                "transaction_id": r[3],
+                "previous_hash": r[4]
+            })
+        conn.commit()
+        return data  # returns list of contracts in a single block of index_block index
+
+    def get_blockchain(self):
+        r = cur.execute(
+            f"SELECT index_block, proof, date, transaction_id, previous_hash FROM blockchain").fetchall()
+        num_blocks = cur.execute(
+            f'SELECT COUNT(DISTINCT index_block) as num_orders FROM blockchain').fetchone()[0]
+        history = [self.get_block(index_block)
+                   for index_block in range(1, num_blocks+1)]
+        return history
+    
+    
+
--- interface_flask.py
+++ interface_flask.py
@@ -1,9 +1,45 @@
-import blockchain, block, transaction
-from flask import Flask, jsonify, request, render_template
+import blockchain
+from block import Block
+from flask import Flask, jsonify, request, render_template, send_file, redirect, url_for
 from uuid import uuid4
+from ecdsa import SigningKey, NIST384p
+from database_manager import contracts_database, blockchain_database
+from PIL import Image, ImageDraw, ImageFont
+import io
+import datetime
+import base64
+from transaction import Transaction
+import os
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from reportlab.pdfgen import canvas
+import hashlib
 
+
+
+blockch = blockchain.Blockchain()
+blockch_database = blockchain_database()
+blk = Block()
+print(blk)
+blockch_database.add_block(blk)
+
+cont_database = contracts_database()
 
 app = Flask(__name__)
+cd = contracts_database()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_data.db'
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    hashed_password = db.Column(db.String(100), nullable=False)
+    signing_key = db.Column(db.String(100), unique=True, nullable=False)
+    
+
+
 
 
 # Generate a globally unique address for this node
@@ -12,68 +48,226 @@ node_identifiere = str(uuid4()).replace('-', '')
 # Instantiate the Blockchain
 blockchaine = blockchain.Blockchain()
 
-@app.route('/', methods=['GET'])
+
+
+
+name = ""
+
+# Route to clear the table
+@app.route('/clear_table')
+def clear_table():
+    try:
+        # Delete all rows from the table
+        User.query.delete()
+
+        # Commit changes
+        db.session.commit()
+        
+        return 'Table cleared successfully'
+    except Exception as e:
+        # Rollback changes if an exception occurs
+        db.session.rollback()
+        return f'Error: {str(e)}'
+
+@app.route('/')
+def index():
+    return render_template('welcome.html')
+
+@app.route('/login')
+def connect():
+    return render_template('login.html')
+
+
+
+@app.route('/login/user', methods=['POST'])
+def login():
+    username = request.form['uname']
+    password = request.form['psw']
+    
+    print(username)
+    print(password)
+
+    user = User.query.filter_by(username=username).first()
+    
+    print(user)
+    
+    print(user.hashed_password)
+    print(generate_password_hash(password))
+
+    if user and check_password_hash(user.hashed_password, password):
+        global name
+        name = username
+        return render_template('home.html' , username=username)
+    else:
+        return "Invalid username or password"
+    
+@app.route('/logout')
+def logout():
+    global name
+    name = ""
+
+    return render_template('logout.html')
+
+
+
+
+
+@app.route('/success')
+def success():
+    return "Login successful!"
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        sk = SigningKey.generate(curve=NIST384p)
+        sk = sk.to_string()
+        sk = sk.hex()  # Convert binary to hexadecimal text
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "Username already exists. Please choose another username."
+        
+        hashed_password = generate_password_hash(password)
+
+        new_user = User(username=username, hashed_password=hashed_password, signing_key=sk)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('connect')) 
+
+    return render_template('register.html')
+
+
+
+
+
+
+@app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/mine', methods=['GET'])
-def mine():
-    return "We'll mine a new Block"
-  
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    values = request.get_json()
-
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
-        return 'Missing values', 400
-
-    # Create a new Transaction
-    index = blockchain.add_transaction(values['sender'], values['recipient'], values['amount'])
-
-    response = {'message': f'Transaction will be added to Block {index}'}
-    return jsonify(response), 201
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': [block.block_data for block in blockchaine.chain],
-        'length': len(blockchaine.chain),
-    }
-    return jsonify(response), 200
-
-
-
-
-@app.route('/create_contract')
-def contract():
+@app.route('/Create_contract')
+def create_contract():
     return render_template('create_contract.html')
 
-# Route to handle form submission and create the contract
+
 @app.route('/create_contract', methods=['POST'])
-def create_contract():
-    # Get form inputs
-    client_name = request.form['client_name']
-    service_description = request.form['service_description']
-    payment_amount = request.form['payment_amount']
-    
-    # Here, you can create the contract using the inputs
-    # For simplicity, let's just format the contract as a string
-    contract = f"Contract\nClient: {client_name}\nDescription of Service: {service_description}\nPayment: ${payment_amount}"
-    
-    # You can then save this contract to a file or database, or perform any other actions needed
-    
-    return contract
+def create():
+    if request.method == 'POST':
+        # Get form inputs
+
+        service_description = request.form['service_description']
+
+        # Here, you can create the contract using the inputs
+        # For simplicity, let's just format the contract as a string
+        contract = f"Description of Service : {service_description}"
+        # You can then save this contract to a file or database, or perform any other actions needed
+        # And now we'll add the transaction in our database
+        tr = Transaction(contract)
+        user = User.query.filter_by(username=name).first()
+        sk = user.signing_key
+        sk = bytes.fromhex(sk)
+        sk = SigningKey.from_string(sk, curve=NIST384p)
+        tr.sign(sk)
+        cd.add_contract(tr)
+        blockch.add_transaction(tr)
+        b= blockch.new_block()
+        b.mine()
+        blockch.extend_chain(b)
+        blockch_database.add_block(b)
+        current_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # Create an image with PIL
+        img = Image.new('RGB', (800, 600), color=(255, 255, 255))
+        draw = ImageDraw.Draw(img)
+        font = ImageFont.load_default()
+
+        # Set margins and starting positions for content
+        margin = 50
+        start_position = 100
+
+        # Write content to the image in a contract-like format
+        contract_content = [
+            f"SERVICE DESCRIPTION: {service_description}"
+        ]
+
+        for content in contract_content:
+            draw.text((margin, start_position), content, fill=(0, 0, 0), font=font)
+            start_position += 50  # Adjust spacing between lines
+
+        # Save the image to a byte buffer as PNG format
+        img_byte_array = io.BytesIO()
+        img.save(img_byte_array, format='PNG')
+        img_byte_array.seek(0)
+        img_base64 = base64.b64encode(img_byte_array.read()).decode('utf-8')
+        return redirect(url_for('contract_display', contract_image=img_base64))
+    return "Something went wrong."
+
+#display the contract image
+@app.route('/contract_display')
+def contract_display():
+    contract_image = request.args.get('contract_image')
+    return render_template('contract_display.html', contract_image=contract_image)
 
 
+@app.route('/Contact')
+def contact():
+    return render_template('contact.html')
 
+@app.route('/blockchain', methods=['GET'])
+def full_chain():
+    blocks = []
+    full_blockchain = blockch_database.get_blockchain()
+    for block in full_blockchain:
+        # Convert rich table to a list of dictionaries
+        block_ = {}
+        block_['title'] = 'Block' + block[0]['index_block']
+        data = []
+        for transaction in block:
+            del transaction['index_block']
+            data.append(transaction)
+        block_['data'] = data
+        blocks.append(block_)
 
+    return render_template('blockchain.html', blocks=blocks)
 
+@app.route('/history')
+def get_user_history():
+    user = User.query.filter_by(username=name).first()
+    sk = user.signing_key
+    sk = bytes.fromhex(sk)
+    sk = SigningKey.from_string(sk, curve=NIST384p)
+    vk = sk.verifying_key.to_pem().hex()
+    author = hashlib.sha256(vk.encode()).hexdigest()
+    history = cont_database.get_history(author)
+    return render_template('history.html', history=history)
+
+@app.route('/generate_pdf/<string:contract_id>')
+def generate_pdf(contract_id):
+    buffer = io.BytesIO()
+    pdf = canvas.Canvas(buffer)
+    contract = cont_database.get_contract(contract_id)
+    pdf.drawString(50, 820, f"Date :  {contract['date']}")
+    pdf.drawString(50, 800, f"Contract Id : {contract['contract_id']}")
+    pdf.drawString(50, 780, f"Author :  {contract['author']}")
+    pdf.drawString(50, 700, f"{contract['content']}")
+    pdf.drawString(50, 100, "Signature : ")
+    pdf.drawString(50, 75, f"{contract['signature'][:64]}")
+    pdf.drawString(50, 50, f"{contract['signature'][64:128]}")
+    pdf.drawString(50, 25, f"{contract['signature'][128:]}")
+    pdf.save()
+
+    # Move the buffer's pointer to the beginning
+    buffer.seek(0)
+
+    # Return the generated PDF file to the user for download
+    return send_file(buffer, as_attachment=True, download_name=f"contract_{contract['contract_id']}.pdf")
 
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
